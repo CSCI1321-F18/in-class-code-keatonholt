4b67444bc9b9eeca14f4647bce5c2854d66e1412
Keaton Holt <kholt@trinity.edu>
2018-08-31 17:02:58 -0500

    Added Dr Mario main object

---
+++ src/main/scala/drmario/DrMario.scala
@@ -0,0 +1,16 @@
+package drmario
+
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.canvas.Canvas
+
+
+object DrMario extends JFXApp{
+  stage = new JFXApp.PrimaryStage {
+     title = "Dr. Mario"
+     scene = new Scene(400, 800) { //8x16 grid
+       val canvas = new Canvas(400, 800)
+       content = canvas
+     }
+   }
+}
